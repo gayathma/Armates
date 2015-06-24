5e1e3f0d971a54cd2c6d31216b81c6d948c12c22
gayathma <gayathma3@gmail.com>
2015-06-24 12:15:30 +0530

    dsf

--- application/views/template/services.php
+++ application/views/template/services.php
@@ -1,11 +1,19 @@
 <div class="element clearfix col3-3 services ">
-    <a href="<?php echo base_url(); ?>uploads/services/ar.png" data-fancybox-group="group1" class="popup">
-        <figure class="images"> <img src="<?php echo base_url(); ?>uploads/services/ar.png"  class="slip" /> </figure>
+    <a href="<?php echo base_url(); ?>uploads/services/ar.png" data-title="Augmented Reality Apps For Your Business" data-fancybox-group="group1" class="popup">
+        <figure class="images"> <img src="<?php echo base_url(); ?>uploads/services/ar.png" alt="<span>Augmented Reality Apps For Your Business</span><i>→</i>" class="slip" /> </figure>
     </a>
 </div>
+<div class="break"></div>
 
 <div class="element clearfix col3-3 services ">
-    <a href="<?php echo base_url(); ?>uploads/services/games.png" data-fancybox-group="group1" class="popup">
-        <figure class="images"> <img src="<?php echo base_url(); ?>uploads/services/games.png"  class="slip" /> </figure>
+    <a href="<?php echo base_url(); ?>uploads/services/games.png" data-title="Game Development" data-fancybox-group="group1" class="popup">
+        <figure class="images"> <img src="<?php echo base_url(); ?>uploads/services/games.png" alt="<span>Game Development</span><i>→</i>"  class="slip" /> </figure>
+    </a>
+</div>
+<div class="break"></div>
+
+<div class="element clearfix col3-3 services ">
+    <a href="<?php echo base_url(); ?>uploads/services/interactive-apps.png" data-title="Promote Your Business With Our Grate Interactive Apps" data-fancybox-group="group1" class="popup">
+        <figure class="images"> <img src="<?php echo base_url(); ?>uploads/services/interactive-apps.png" alt="<span>Promote Your Business With Our Grate Interactive Apps</span><i>→</i>" class="slip" /> </figure>
     </a>
 </div>
